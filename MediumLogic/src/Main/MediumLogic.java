0608aad60efbd0fb31a5ff0904f95db96d16a51f
Alexander Stämmler <Alexander.Staemmler@Student.HTW-Berlin.de>
2021-04-25 14:41:05 +0200

    Added solution for makeChocolate and modified main method.

--- MediumLogic/src/Main/MediumLogic.java
+++ MediumLogic/src/Main/MediumLogic.java
@@ -3,7 +3,8 @@ package Main;
 public class MediumLogic {
 
 	public static void main(String[] args) {
-
+		System.out.println(closeFar(5, 1, 10));
+		System.out.println(makeChocolate(5, 1, 10));
 	}
 
 	// closeFar
@@ -12,7 +13,7 @@ public class MediumLogic {
 	// 1), while the other is "far", differing from both other values by 2 or more.
 	// Note:
 	// Math.abs(num) computes the absolute value of a number.
-	public boolean closeFar(int a, int b, int c) {
+	public static boolean closeFar(int a, int b, int c) {
 		if (Math.abs(a - b) <= 1 && Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2) {
 			return true;
 		} else if (Math.abs(a - c) <= 1 && Math.abs(a - b) >= 2 && Math.abs(b - c) >= 2) {
@@ -23,7 +24,15 @@ public class MediumLogic {
 	}
 
 	// makeChocolate
-	public int makeChocolate(int small, int big, int goal) {
-
+	// Make a package of goal kilos of chocolate. Use small bars (1 kilo each) and big bars (5 kilos each).
+	// Return the number of small bars to use, assuming we always use big bars before small bars.
+	// Return -1 if it can't be done.
+	public static int makeChocolate(int small, int big, int goal) {
+		int sum = 0;
+		if ((goal%5) == 0 && goal <= (big*5)) return 0;
+		for (int i = 5; i < goal; i += 5) sum = i;
+		if (sum > (big*5)) sum = big * 5;
+		if ((goal - sum) <= small) return goal - sum;
+		return -1;
 	}
 }
